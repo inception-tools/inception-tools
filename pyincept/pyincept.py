9c80257c99ea537916b52052cac5f7547dafbef4
Andrew van Herick <andrew.vanherick@gmail.com>
2020-06-26 12:36:50 -0700

    GH-7: Update docstring comments for pyincept.py.

--- pyincept/pyincept.py
+++ pyincept/pyincept.py
@@ -2,19 +2,37 @@
     pyincept
     ~~~~~~~~~~~~~~~~~~~~~~~
 
-    Main entry point commend line script.  Command line syntax:
+    Main entry point commend line script.  See :py:func:`main` for details of
+    how to invoke this script.
 
-    ::
+    ~~~~~~~~~~~~~~~~~~~~~~~
 
-        pyincept <package-name> <author-name> <author-email>
+    Unpublished Copyright 2020 Andrew van Herick. All Rights Reserved.
+"""
+
+__author__ = 'Andrew van Herick'
+
+from datetime import datetime
+
+import click
+
+from pyincept.project_builder import ProjectBuilder
+
+
+@click.command()
+@click.argument('package_name')
+@click.argument('author')
+@click.argument('author_email')
+def main(package_name, author, author_email):
+    """
+     Main entry point commend line script.  Command line syntax:
+
+        pyincept my_package <author-name> <author-email>
 
     Invoking the command line above will result in the creation of a
     directory with the following structure:
 
-        will result in the creation of a directory structure as follows:
-
-    ::
-
+    \b
         <working-dir>/
             my_package/
                 my_package/
@@ -44,40 +62,16 @@
     where many of the files are parameterize with the package name, author
     name, author email, etc.
 
-    ~~~~~~~~~~~~~~~~~~~~~~~
+    PACKAGE_NAME: the name of the package to be incepted.  This should be
+    the package name, as you would expect it to appear in code references (
+    e.g. 'my_package' and not 'my-package'
 
-    Unpublished Copyright 2020 Andrew van Herick. All Rights Reserved.
-"""
-
-__author__ = 'Andrew van Herick'
-
-from datetime import datetime
-
-import click
-
-from pyincept.project_builder import ProjectBuilder
-
-
-@click.command()
-@click.argument('package_name')
-@click.argument('author')
-@click.argument('author_email')
-def main(package_name, author, author_email):
-    """
-    This function represents the main command-line entry point of the
-    `pyincept` project.  See the module-level documentation for how to
-    invoke this method from the command line.
-
-    :param package_name: the name of the package to be incepted.  This
-    should be the package name, as you would expect it to appear in code
-    references (e.g. 'my_package' and not 'my-package'
-
-    :param author: the name of the package author.  This string is used in
+    AUTHOR: the name of the package author.  This string is used in
     copyright notices, setup.py package metadata, and for __author__
     assignments in Python stub files.
 
-    :param author_email: the email of the package author.  This is used in
-    setup.py package metadata and in the auto-generated boiler-plate text of
+    AUTHOR_EMAIL: the email of the package author.  This is used in setup.py
+    package metadata and in the auto-generated boiler-plate text of
     README.rst file.
     """
     builder = ProjectBuilder(
