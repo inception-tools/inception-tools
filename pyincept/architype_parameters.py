58c3f2f9f2265e02977f9fc3e881da5433cf2f72
Andrew van Herick <andrew.vanherick@gmail.com>
2020-07-05 11:02:41 -0700

    GH-19: Fix broken support for Python 3.5.

--- pyincept/architype_parameters.py
+++ pyincept/architype_parameters.py
@@ -1,26 +1,20 @@
 """
     architype_parameters
     ~~~~~~~~~~~~~~~~~~~~~~~
+
     Houses the declaration of :py:class:`ArchitypeParameters` along with
     supporting classes, functions, and attributes.
+
     ~~~~~~~~~~~~~~~~~~~~~~~
+
     Unpublished Copyright 2020 Andrew van Herick. All Rights Reserved.
 """
 
 __author__ = 'Andrew van Herick'
 
-from datetime import datetime
-from typing import NamedTuple
+from collections import namedtuple
 
-
-# Python >= 3.6
-class ArchitypeParameters(NamedTuple):
-    """
-    The 'container class' defines and encapsulates the set of parameters which
-    can be used to render and save an :py:class:`Architype`.  Instances of this
-    class are intended to be immutable.
-    """
-    package_name: str
-    author: str
-    author_email: str
-    date: datetime.date
+ArchitypeParameters = namedtuple(
+    'ArchitypeParameters',
+    ('package_name', 'author', 'author_email', 'date')
+)
