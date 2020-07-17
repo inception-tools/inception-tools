4eb169d0213fbfedef99df36ea68ebffc8f3706e
Andrew van Herick <andrew.vanherick@gmail.com>
2020-07-17 07:57:49 -0700

    pyincept-37: Use a consistent convention for declaring docstring comments.

--- pyincept/standard_archetype.py
+++ pyincept/standard_archetype.py
@@ -41,39 +41,41 @@ class StandardArchetype(Archetype, Enum, metaclass=_ABCEnumMeta):
     across the system.
     """
 
-    #: The :py:meth:`build` method of this :py:class:`Archetype` will create a
-    # directory/file tree with the following structure:
-    #:
-    #: ::
-    #:
-    #:     root_dir/
-    #:         my_package/
-    #:             __init__.py
-    #:             my_package.py
-    #:         tests/
-    #:             __init__.py
-    #:             end-to-end/
-    #:                 __init__.py
-    #:                 test_my_package/
-    #:                     __init__.py
-    #:             integration/
-    #:                 __init__.py
-    #:                 test_my_package/
-    #:                     __init__.py
-    #:             unit/
-    #:                 __init__.py
-    #:                 test_my_package/
-    #:                     __init__.py
-    #:         LICENSE
-    #:         Makefile
-    #:         Pipfile
-    #:         README.rst
-    #:         setup.cfg
-    #:         setup.py
-    #:
-    #: where 'root_dir' is the `root_dir argument and 'my_package' is the
-    # `package_name` attribute of the params argument.
     APPLICATION = ('pyincept-archetype-application',)
+    """
+    The :py:meth:`build` method of this :py:class:`Archetype` will create a
+    directory/file tree with the following structure:
+
+    ::
+
+        root_dir/
+            my_package/
+                __init__.py
+                my_package.py
+            tests/
+                __init__.py
+                end-to-end/
+                    __init__.py
+                    test_my_package/
+                        __init__.py
+                integration/
+                    __init__.py
+                    test_my_package/
+                        __init__.py
+                unit/
+                    __init__.py
+                    test_my_package/
+                        __init__.py
+            LICENSE
+            Makefile
+            Pipfile
+            README.rst
+            setup.cfg
+            setup.py
+
+    where 'root_dir' is the `root_dir argument and 'my_package' is the
+    `package_name` attribute of the params argument.
+    """
 
     def __init__(self, architype_resource_id) -> None:
         # Referencing ArchetypeBase directly for the sake of supporting Python
