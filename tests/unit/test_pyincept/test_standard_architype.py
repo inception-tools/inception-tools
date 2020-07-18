2aacda517109cde03f0671e401e9568274655d99
Andrew van Herick <andrew.vanherick@gmail.com>
2020-07-18 14:41:10 -0700

    pyincept-37: Refactoring: Consolidate the test cases for StandardArchetype under an enumerated list rather than have a separate test case for each output file.

--- tests/unit/test_pyincept/test_standard_architype.py
+++ tests/unit/test_pyincept/test_standard_architype.py
@@ -41,107 +41,39 @@ class TestStandardArchetype(PyinceptTestBase):
     )
 
     _PACKAGE_NAME = PyinceptTestBase._PACKAGE_NAME
-    _EXPECTED_OUTPUT = (
-        _TestOutput('scripts', None),
-        _TestOutput('docs', None),
-        _TestOutput('LICENSE', os.path.join(_TEST_RESOURCE_PATH, 'LICENSE')),
-        _TestOutput(
-            'README.rst',
-            os.path.join(_TEST_RESOURCE_PATH, 'README.rst')
-        ),
-        _TestOutput(
-            'setup.cfg',
-            os.path.join(_TEST_RESOURCE_PATH, 'setup.cfg')
-        ),
-        _TestOutput('setup.py', os.path.join(_TEST_RESOURCE_PATH, 'setup.py')),
-        _TestOutput('log.cfg', os.path.join(_TEST_RESOURCE_PATH, 'log.cfg')),
-        _TestOutput('Makefile', os.path.join(_TEST_RESOURCE_PATH, 'Makefile')),
-        _TestOutput('Pipfile', os.path.join(_TEST_RESOURCE_PATH, 'Pipfile')),
-        _TestOutput(
-            os.path.join(_PACKAGE_NAME, 'main.py'),
-            os.path.join(_TEST_RESOURCE_PATH, _PACKAGE_NAME, 'main.py')
-        ),
-        _TestOutput(
-            os.path.join(_PACKAGE_NAME, '__init__.py'),
-            os.path.join(_TEST_RESOURCE_PATH, _PACKAGE_NAME, '__init__.py')
-        ),
-        _TestOutput(
-            os.path.join('tests', '__init__.py'),
-            os.path.join(_TEST_RESOURCE_PATH, 'tests', '__init__.py')
-        ),
-        _TestOutput(
-            os.path.join('tests', 'end_to_end', '__init__.py'),
-            os.path.join(
-                _TEST_RESOURCE_PATH,
-                'tests',
-                'end_to_end',
-                '__init__.py'
-            )
-        ),
-        _TestOutput(
-            os.path.join('tests', 'integration', '__init__.py'),
-            os.path.join(
-                _TEST_RESOURCE_PATH,
-                'tests',
-                'integration',
-                '__init__.py'
-            )
-        ),
-        _TestOutput(
-            os.path.join('tests', 'unit', '__init__.py'),
-            os.path.join(_TEST_RESOURCE_PATH, 'tests', 'unit', '__init__.py')
-        ),
-        _TestOutput(
-            os.path.join(
-                'tests',
-                'end_to_end',
-                'test_some_package_name',
-                '__init__.py'
-            ),
-            os.path.join(
-                _TEST_RESOURCE_PATH,
-                'tests',
-                'end_to_end',
-                'test_some_package_name',
-                '__init__.py'
-            )
-        ),
-        _TestOutput(
-            os.path.join(
-                'tests',
-                'integration',
-                'test_some_package_name',
-                '__init__.py'
-            ),
-            os.path.join(
-                _TEST_RESOURCE_PATH,
-                'tests',
-                'integration',
-                'test_some_package_name',
-                '__init__.py'
-            )
-        ),
-        _TestOutput(
-            os.path.join(
-                'tests',
-                'unit',
-                'test_some_package_name',
-                '__init__.py'
-            ),
-            os.path.join(
-                _TEST_RESOURCE_PATH,
-                'tests',
-                'unit',
-                'test_some_package_name',
-                '__init__.py'
-            )
-        ),
+
+    _EXPECTED_DIRS = ('scripts', 'docs')
+
+    _EXPECTED_FILES = (
+        ('LICENSE',),
+        ('README.rst',),
+        ('setup.cfg',),
+        ('setup.py',),
+        ('log.cfg',),
+        ('Makefile',),
+        ('Pipfile',),
+        (_PACKAGE_NAME, '__init__.py'),
+        (_PACKAGE_NAME, 'main.py'),
+        ('tests', '__init__.py'),
+        ('tests', 'end_to_end', '__init__.py'),
+        ('tests', 'integration', '__init__.py'),
+        ('tests', 'unit', '__init__.py'),
+        ('tests', 'end_to_end', 'test_' + _PACKAGE_NAME, '__init__.py'),
+        ('tests', 'integration', 'test_' + _PACKAGE_NAME, '__init__.py'),
+        ('tests', 'unit', 'test_' + _PACKAGE_NAME, '__init__.py'),
     )
 
-    ##############################
-    # Instance methods
-
-    # Test cases
+    @classmethod
+    def _expected_output(cls):
+        expected_dirs = tuple(_TestOutput(s, None) for s in cls._EXPECTED_DIRS)
+        expected_files = tuple(
+            _TestOutput(
+                os.path.join(*s),
+                os.path.join(cls._TEST_RESOURCE_PATH, *s)
+            )
+            for s in cls._EXPECTED_FILES
+        )
+        return expected_dirs + expected_files
 
     def test_output_files(self):
         """
@@ -150,7 +82,7 @@ class TestStandardArchetype(PyinceptTestBase):
         """
         expected = (
             os.path.join(self._ROOT_DIR, j.subpath)
-            for j in self._EXPECTED_OUTPUT
+            for j in self._expected_output()
         )
         actual = StandardArchetype.APPLICATION.output_files(
             self._ROOT_DIR,
@@ -163,4 +95,7 @@ class TestStandardArchetype(PyinceptTestBase):
         Unit test case for :py:method:`StandardArchetype.APPLICATION.build`.
         """
         StandardArchetype.APPLICATION.build(self._ROOT_DIR, self._PARAMS)
-        self._validate_archetype_output(self._ROOT_DIR, self._EXPECTED_OUTPUT)
+        self._validate_archetype_output(
+            self._ROOT_DIR,
+            self._expected_output()
+        )
